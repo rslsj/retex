1488763ad26fabe74602d2b07b91e4fa3bd509e0
Josua Rosales <josuarosales1798@gmail.com>
2024-09-13 23:30:55 +0200

    feat: introduced mobile component for mobile viewport

---
+++ components/Home/GroupArticle/Secondary/Mobile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bb p-6 flex-col">
+    <div class="h-[358px] w-full bg-woman"></div>
+    <common-article class="mt-6" :article="article" :text-color="'#000000'" />
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  article: {
+    type: Object,
+  },
+});
+</script>
+<style scoped>
+.bg-woman {
+  background-image: url("/assets/articles/woman.jpg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
