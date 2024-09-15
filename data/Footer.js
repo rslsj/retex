a348b7517affe56fc4413f3e3d49863fc4f419de
Josua Rosales <josuarosales1798@gmail.com>
2024-09-15 20:32:27 +0200

    fea: introduced footer data

---
+++ data/Footer.js
@@ -0,0 +1,30 @@
+export const items = [
+  {
+    text: "Il Gruppo",
+    link: "/",
+  },
+  {
+    text: "Note Legali",
+    link: "/",
+  },
+  {
+    text: "Chi Siamo",
+    link: "/",
+  },
+  {
+    text: "Condizioni d'uso",
+    link: "/",
+  },
+  {
+    text: "Privacy",
+    link: "/",
+  },
+  {
+    text: "Scrivici",
+    link: "/",
+  },
+  {
+    text: "Pubblicità",
+    link: "/",
+  },
+];
