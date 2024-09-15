eae25d77259c11a7c5b79c58f027573317da3c9a
Josua Rosales <josuarosales1798@gmail.com>
2024-09-15 16:11:45 +0200

    feat: inotrduced bloggers data

---
+++ data/Bloggers.js
@@ -0,0 +1,44 @@
+export const bloggers = [
+  {
+    cartTitle: "Il Blog",
+    title: null,
+    img: null,
+    author: null,
+    mobile: null,
+  },
+  {
+    cartTitle: null,
+    title: "Il Becco dell’oca",
+    img: "/assets/bloggers/a1.png",
+    author: "Di Paolo Dell’Oca",
+    mobile: null,
+  },
+  {
+    cartTitle: null,
+    title: "Disarmato",
+    img: "/assets/bloggers/a2.png",
+    author: "Di Pasquale Pugliese",
+    mobile: null,
+  },
+  {
+    cartTitle: null,
+    title: "Battitiperminuto",
+    img: "/assets/bloggers/a3.png",
+    author: "Di Lorenzo Maria Alvaro",
+    mobile: null,
+  },
+  {
+    cartTitle: null,
+    title: "La Zanzarella",
+    img: "/assets/bloggers/a4.png",
+    author: "Di Elena Zanella",
+    mobile: null,
+  },
+  {
+    cartTitle: null,
+    title: "Impact Q&A",
+    img: "/assets/bloggers/a5.png",
+    author: "Di Laura Orestano",
+    mobile: "Tutti i blog",
+  },
+];
