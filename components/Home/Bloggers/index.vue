5c06c1e11cd56ca297cb609dd7aefb99e78a64d4
Josua Rosales <josuarosales1798@gmail.com>
2024-09-15 21:33:26 +0200

    fix: authors borders

--- components/Home/Bloggers/index.vue
+++ components/Home/Bloggers/index.vue
@@ -1,8 +1,31 @@
 <template>
   <div class="grid grid-cols-2 md:grid-cols-6">
-    <home-bloggers-item class="sbb" v-for="blog in bloggers" :blog="blog" />
+    <home-bloggers-item
+      v-for="(blog, index) in bloggers"
+      :blog="blog"
+      :class="[
+        'border-black',
+        !isMd && index !== 5
+          ? 'border-t border-r border-b'
+          : ' border-t border-b border-r-0',
+        isMd && index % 2 === 0 ? 'br border-b-0' : 'border-r-1 border-b-0',
+      ]"
+    />
   </div>
 </template>
 <script setup>
 import { bloggers } from "@/data/Bloggers.js";
+const isMd = ref(window?.innerWidth < 768);
+
+const handleResize = () => {
+  isMd.value = window?.innerWidth < 768;
+};
+
+onMounted(() => {
+  window.addEventListener("resize", handleResize);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", handleResize);
+});
 </script>
