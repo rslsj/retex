a4b4e168e5971d130878836c72b0db3db72456fe
Josua Rosales <josuarosales1798@gmail.com>
2024-09-15 23:00:45 +0200

    fix: height of navbar footer

--- components/Common/Navbar/Menu/FooterItem.vue
+++ components/Common/Navbar/Menu/FooterItem.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink :to="props.link">
     <div
-      class="flex justify-center items-center py-8 bg-white hover:bg-[#f5f5f5] cursor-pointer px-4"
+      class="flex justify-center items-center py-8 bg-white hover:bg-[#f5f5f5] cursor-pointer px-4 max-h-[88px]"
     >
       {{ props.text }}
     </div>
