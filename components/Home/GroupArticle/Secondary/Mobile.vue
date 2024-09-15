c237964524d03c077eeabb0dc24e27b85f392f72
Josua Rosales <josuarosales1798@gmail.com>
2024-09-15 22:11:04 +0200

    feat: introduce label to the mobile picture

--- components/Home/GroupArticle/Secondary/Mobile.vue
+++ components/Home/GroupArticle/Secondary/Mobile.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="bb p-6 flex-col">
-    <div class="h-[358px] w-full bg-woman"></div>
+    <div
+      class="h-[358px] w-full bg-woman text-white p-6 flex flex-col justify-end text-[32px]"
+    >
+      <h3 class="font-extralight">Intervista a</h3>
+      <h2 class="font-bold">Mariangela Cassano</h2>
+    </div>
     <common-article class="mt-6" :article="article" :text-color="'#000000'" />
   </div>
 </template>
